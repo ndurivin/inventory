62fa3ac6408eed98c9badacbf175d68dbc453de5
Vincent Nduri <ndurivo@gmail.com>
2023-03-07 20:07:40 +0300

    Adds the categories controller

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'categories/index'
+      get 'categories/show'
+      get 'categories/create'
+      get 'categories/update'
+      get 'categories/destroy'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
