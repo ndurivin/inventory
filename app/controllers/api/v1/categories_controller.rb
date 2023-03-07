ff55f4483b753b2c3c63b134589f063387e153c6
Vincent Nduri <ndurivo@gmail.com>
2023-03-07 22:06:16 +0300

    Adds the controller  CRUD methods

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,19 +1,58 @@
 class Api::V1::CategoriesController < ApplicationController
   def index
+    categories = Category.all
+    render json: categories, status: 200
   end
 
   def show
+    category = category.find_by(id: params[:id])
+    if category
+      render json: category, status: 200
+    else
+      render json: {
+        error: "Category not found.."
+      }
+    end
   end
 
   def create
+    category = Category.new(
+      name: category_params[:name]
+    )
+    if category.save
+      render json: category, status: 200
+    else
+      render json: {
+        error: 'Error creating category...'
+      }
+    end
   end
 
   def update
+    category = Category.find_by(id: params[id])
+    if category
+      category.update(name: params[:name])
+       render json: "Category updated successfully"
+    else
+      render json: {
+        error: "Category not found"
+      }
+    end
   end
 
   def destroy
+    category = Category.find_by(id: params[id])
+    if category
+      category.destroy
+      render json: "Category deleted successfully"
+    end
   end
 
   private
+  def category_params
+    params.require(:category).permit([
+      :name
+    ])
+  end
   
 end
