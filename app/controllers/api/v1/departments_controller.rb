40886c28bb9158dc6221e5963ac016ba92b6b99c
Vincent Nduri <ndurivo@gmail.com>
2023-03-08 10:02:03 +0300

    Configure department controller methods & routes

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -1,16 +1,56 @@
 class Api::V1::DepartmentsController < ApplicationController
+ 
   def index
+    departments = Department.all
+    render json: departments, status: 200
   end
 
   def show
+    department = Department.find_by(id: params[:id])
+    if department
+      render json: department, status: 200
+    else
+      render json: {
+        error: "Department not found.."
+      }
+    end
   end
 
   def create
+    department = Department.new(
+      name: department_params[:name]
+    )
+    if Department.save
+      render json: department, status: 200
+    else
+      render json: {
+        error: 'Error creating Department...'
+      }
+    end
   end
 
   def update
+    department = Department.find_by(id: params[:id])
+    if department
+      department.update(name: params[:name])
+       render json: "Department updated successfully"
+    else
+      render json: {
+        error: "Department not found"
+      }
+    end
   end
 
   def destroy
+    department = Department.find_by(id: params[id])
+    if department
+      Department.destroy
+      render json: "Department deleted successfully"
+    end
+  end
+
+  private
+  def department_params
+    params.require(:department).permit([:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'departments/index'
-      get 'departments/show'
-      get 'departments/create'
-      get 'departments/update'
-      get 'departments/destroy'
+      resources :departments, only: [:index, :show, :create, :update, :destroy]
       resources :categories, only: [:index, :show, :create, :update, :destroy]
     end
   end
