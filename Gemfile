e9b9a776dd348b6ad933dfbaa703ac4336a84785
Vincent Nduri <ndurivo@gmail.com>
2023-03-15 20:53:12 +0300

    Adds devise and creates user model

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.0"
 gem 'active_model_serializers', '~> 0.10.13'
+gem 'devise', '~> 4.9'
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
--- app/controllers/api/v1/assets_controller.rb
+++ app/controllers/api/v1/assets_controller.rb
@@ -1,16 +1,58 @@
 class Api::V1::AssetsController < ApplicationController
+ 
   def index
+    assets = Asset.all
+    render json: assets, status: 200
   end
 
   def show
+    asset = Asset.find_by(id: params[:id])
+    if asset
+      render json: asset, status: 200
+    else
+      render json: {
+        error: "Asset not found..!"
+      }
+    end
   end
 
+  
   def create
+    asset = Asset.new(
+      name: asset_params[:name, :model, :brand, :image_url, :color, :state]
+    )
+    if asset.save
+      render json: asset, status: 200
+    else
+      render json: {
+        error: 'Error creating Asset...!'
+      }
+    end
   end
 
   def update
+    asset = Asset.find_by(id: params[:id])
+    if asset
+      asset.update(name: params[:name])
+       render json: "Asset updated successfully!"
+    else
+      render json: {
+        error: "Asset not found"
+      }
+    end
   end
 
   def destroy
+    asset =Asset.find_by(id: params[id])
+    if asset
+      Asset.destroy
+      render json: "Asset deleted successfully!"
+    end
   end
+
+  private
+  def asset_params
+    params.require(:asset).permit([:name, :model, :brand, :image_url, :color, :state])
+  end
+
 end
